e89438bccdd992b3c78d3fc57f7c62d57c2a2149
Julian Kranz <kranzj@in.tum.de>
2014-08-18 18:24:44 +0200

    ---

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cppgdsl/gdsl.h>
+#include <cppgdsl/frontend/bare_frontend.h>
+#include <cppgdsl/instruction.h>
+#include <cppgdsl/rreil/statement/statement.h>
+
+using namespace gdsl::rreil;
+
 int main(void) {
-	return 0;
+  gdsl::bare_frontend f("current");
+  gdsl::gdsl g(&f);
+
+  uint16_t buffer = 0x0000;
+  g.set_code((char*)&buffer, sizeof(buffer), 0);
+
+  gdsl::instruction insn = g.decode();
+
+  printf("Instruction: %s\n", insn.to_string().c_str());
+  printf("---------------------------------\n");
+
+  auto rreil = insn.translate();
+
+  g.reset_heap();
+
+  printf("RReil:\n");
+  for(statement *s : *rreil)
+    printf("%s\n", s->to_string().c_str());
+
+  for(statement *s : *rreil)
+    delete s;
+  delete rreil;
+
+  return 0;
 }
