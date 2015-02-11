0d0610e21e3179f3e104e95c4df3f8e434eb4f6b
Julian Kranz <kranzj@in.tum.de>
2015-02-11 13:01:17 +0100

    Value sets

---
+++ include/summy/rreil/sexpr/value_set.h
@@ -0,0 +1,31 @@
+/*
+ * value_set.h
+ *
+ *  Created on: Feb 11, 2015
+ *      Author: jucs
+ */
+
+#pragma once
+#include <cppgdsl/rreil/sexpr/sexpr.h>
+#include <cppgdsl/rreil/sexpr/sexpr_visitor.h>
+#include <iostream>
+
+namespace summy {
+namespace rreil {
+
+enum vsb_type {
+  INFINITY, CONSTANT
+};
+
+struct vs_boundary {
+};
+
+class value_set : public gdsl::rreil::sexpr {
+private:
+  void put(std::ostream &out);
+public:
+  void accept(gdsl::rreil::sexpr_visitor &v);
+};
+
+}
+}
