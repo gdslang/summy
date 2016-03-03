458ec1a21700475ae6eb69a6099ae0d2dd1f995f
Julian Kranz <kranzj@in.tum.de>
2016-03-03 18:48:49 +0100

    Small fix

--- src/big_step/dectran.cpp
+++ src/big_step/dectran.cpp
@@ -5,6 +5,7 @@
  *      Author: Julian Kranz
  */
 
+#include <cppgdsl/gdsl_exception.h>
 #include <summy/cfg/node/node_visitor.h>
 #include <summy/cfg/node/address_node.h>
 #include <summy/transformers/transformer.h>
@@ -13,6 +14,7 @@
 #include <summy/transformers/ip_propagator.h>
 #include <summy/transformers/trivial_connector.h>
 #include <cppgdsl/gdsl.h>
+#include <cppgdsl/gdsl_exception.h>
 #include <cppgdsl/block.h>
 #include <cppgdsl/optimization.h>
 #include <cppgdsl/instruction.h>
@@ -20,6 +22,8 @@
 #include <limits.h>
 #include <summy/big_step/dectran.h>
 #include <vector>
+
+using gdsl::gdsl_exception;
 
 using namespace gdsl::rreil;
 using namespace std;
@@ -39,15 +43,24 @@ cfg::translated_program_t dectran::decode_translate(bool decode_multiple) {
         LONG_MAX);
       rreil = b.get_statements();
     } else {
-      gdsl::instruction insn = gdsl.decode();
-//            printf("Instruction: %s\n", insn.to_string().c_str());
-//            printf("---------------------------------\n");
-      rreil = insn.translate();
+      optional<gdsl::instruction> insn = [&]() -> optional<gdsl::instruction> {
+        try {
+          return optional<gdsl::instruction>(gdsl.decode());
+        } catch(gdsl_exception &s) {
+          cout << "Decoding error @0x" << hex << gdsl.get_ip() << dec << ": " << s << endl;
+          gdsl.reset_heap();
+          return nullopt;
+        }
+      }();
+      if(!insn) continue;
+      //            printf("Instruction: %s\n", insn.to_string().c_str());
+      //            printf("---------------------------------\n");
+      rreil = insn.value().translate();
     }
 
     gdsl.reset_heap();
 
-//    printf("RReil (no transformations):\n");
+    //    printf("RReil (no transformations):\n");
     //    for(statement *s : *rreil)
     //      printf("%s\n", s->to_string().c_str());
 
@@ -69,23 +82,23 @@ size_t dectran::initial_cfg(cfg::cfg &cfg, bool decode_multiple, std::experiment
     delete rreil;
   }
 
-//  vector<transformer *> transformers;
-//  transformers.push_back(new decomposer(&cfg, head_node));
-//  transformers.push_back(new goto_ip_adder(&cfg, head_node));
-//  transformers.push_back(new ip_propagator(&cfg, head_node));
-//  //  transformers.push_back(new trivial_connector(&cfg));
-//  for(auto t : transformers) {
-//    t->transform();
-//    delete t;
-//  }
-//
-//  /*
-//   * We're not interested in trivial updates...
-//   */
-//  cfg.clear_updates();
+  //  vector<transformer *> transformers;
+  //  transformers.push_back(new decomposer(&cfg, head_node));
+  //  transformers.push_back(new goto_ip_adder(&cfg, head_node));
+  //  transformers.push_back(new ip_propagator(&cfg, head_node));
+  //  //  transformers.push_back(new trivial_connector(&cfg));
+  //  for(auto t : transformers) {
+  //    t->transform();
+  //    delete t;
+  //  }
+  //
+  //  /*
+  //   * We're not interested in trivial updates...
+  //   */
+  //  cfg.clear_updates();
 
   return head_node;
 }
 
 dectran::dectran(cfg::cfg &cfg, gdsl::gdsl &gdsl, bool blockwise_optimized)
-    : cfg(cfg), blockwise_optimized(blockwise_optimized), gdsl(gdsl){}
+    : cfg(cfg), blockwise_optimized(blockwise_optimized), gdsl(gdsl) {}
