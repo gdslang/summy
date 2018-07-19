1aecdb24cede729314d439ac6998f1e1e827e113
Julian Kranz <kranzj@in.tum.de>
2018-07-19 22:01:50 +0200

    ---

---
+++ test_programs/test_appl1.c
@@ -0,0 +1,16 @@
+void f(int *p) {
+  *p += 1;
+}
+
+int main(int argc, char **argv) {
+  int x = 1;
+  int y = 2;
+  int *p = &y;
+  //int *p;
+  //if(argc > 3)
+  //  p = &x;
+  //else
+  //  p = &y;
+  f(p);
+  return *p;
+}
